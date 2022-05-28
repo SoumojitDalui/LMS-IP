eae13e467132e6515bb42e19591fe01aa8e0b75e
Soumojit Dalui <39549918+SoumojitDalui@users.noreply.github.com>
2022-05-28 20:59:13 +0530

    Form Extra Commit

--- src/Components/LeaveForm.js
+++ src/Components/LeaveForm.js
@@ -1,10 +1,13 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { useForm } from "react-hook-form";
+import moment from 'moment';
 import { add } from '../redux/leaves/leaveSlice'
 import { Form, Button } from 'react-bootstrap'
 
-function LeaveForm(handleChange) {
+function LeaveForm() {
+    const [startDateInvalid, setStartDateInvalid] = React.useState(false);
+    const [endDateInvalid, setEndDateInvalid] = React.useState(false);
     const leaveData = useSelector(state => state.leaves.LEAVES)
     const dispatch = useDispatch()
     let newid = checkId(1)
@@ -29,9 +32,30 @@ function LeaveForm(handleChange) {
 
     const startVal = watch('start')
     const endVal = watch('end')
+
+    useEffect(() => {
+        if (startVal) {
+            leaveData.every(info => {
+                if (moment(startVal).isBetween(info.start, info.end) || moment(startVal).isSame(info.start) || moment(startVal).isSame(info.end)) {
+                    setStartDateInvalid(true)
+                    return false
+                }
+                setStartDateInvalid(false)
+            })
+        }
+        if (endVal) {
+            leaveData.every(info => {
+                if (moment(endVal).isBetween(info.start, info.end) || moment(endVal).isSame(info.start) || moment(endVal).isSame(info.end)) {
+                    setEndDateInvalid(true)
+                    return false
+                }
+                setEndDateInvalid(false)
+            })
+        }
+    }, [leaveData, startVal, endVal])
     
     const handleFormSubmit = (data) => {
-        if (startVal <= endVal) {
+        if (startVal <= endVal && !startDateInvalid && !endDateInvalid) {
             const newData = { ...data, id: newid }
             dispatch(add(newData));
             reset()
@@ -44,13 +68,15 @@ function LeaveForm(handleChange) {
                 <Form.Group className='mb-3'>
                     <Form.Label>Start Date: </Form.Label>
                     <Form.Control type='date' name='start' watch="start" {...register("start", {required: "Required",})} />
-                    {errors.start && <span>This field is required</span>}
+                    {errors.start && <><Form.Text style={{color: 'red'}}>This field is required</Form.Text><br /></>}
+                    {(startVal && startDateInvalid) && <><Form.Text style={{color: 'red'}}>Date is already selected in leave</Form.Text></>}
                 </Form.Group>
                 <Form.Group className='mb-3'>
                     <Form.Label>End Date: </Form.Label>
                     <Form.Control type="date" name="end" watch="end" {...register("end", {required: "Required",})} />
-                    {errors.end && <span>This field is required</span>}
-                    {startVal && endVal && startVal > endVal && <span>End date must be greater than start date</span>}
+                    {errors.end && <><Form.Text style={{color: 'red'}}>This field is required</Form.Text><br /></>}
+                    {startVal && endVal && startVal > endVal && <><Form.Text style={{color: 'red'}}>End date must be greater than Start date</Form.Text><br /></>}
+                    {(endVal && endDateInvalid) && <><Form.Text style={{color: 'red'}}>Date is already selected in leave</Form.Text></>}
                 </Form.Group>
                 <Form.Group className='mb-3'>
                     <Form.Label>Reason: </Form.Label>
@@ -61,7 +87,7 @@ function LeaveForm(handleChange) {
                         <option value="Client Leave">Client Leave</option>
                         <option value="Public Leave">Public Leave</option>
                     </Form.Select>
-                    {errors.reason && <span>This field is required</span>}
+                    {errors.reason && <Form.Text style={{color: 'red'}}>This field is required</Form.Text>}
                 </Form.Group>
                 <Button className='btn btn-primary' style={{marginRight: '1em'}} type="submit">Submit</Button>
                 <Button className='btn btn-warning' type="button" onClick={() => reset()}>Reset</Button>
